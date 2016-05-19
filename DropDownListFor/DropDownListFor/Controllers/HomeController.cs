ebeff7a1acd5c4729a8249056441f44a790675d4
yuanshenghuang <yuansheng@live.be>
2016-05-19 19:01:15 +0200

    j

---
+++ DropDownListFor/DropDownListFor/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using DropDownListFor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DropDownListFor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var category = new Category();
+            category.CategoryList = new[]
+                {
+                    new SelectListItem{Value="opel" ,Text="opel"},
+                    new SelectListItem{Value="Mercedez", Text ="Mercedez"},
+                    new SelectListItem{Value ="BMW", Text="BMW"}
+                };
+            return View(category);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Category category)
+        {
+            return Content("Thanks for selecting: " + category.SelectedCategory);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DropDownListFor/DropDownListFor/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DropDownListFor.Models
+{
+    public class Category
+    {
+        [DisplayName("Select a category")]
+        public string SelectedCategory { get; set; }
+
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+
+    }
+}
